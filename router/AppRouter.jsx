abed00a5757962488ddf396a1db8416447b12419
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2024-09-02 19:31:58 -0600

    First Commit

---
+++ router/AppRouter.jsx
@@ -0,0 +1,37 @@
+import { Route, Routes} from 'react-router-dom';
+import { LoginPages } from '../auth';
+import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
+
+export const AppRouter = () => {
+    
+    return(
+        <>
+
+            <Routes>
+               
+                <Route path="/login"
+                        element ={
+                            <PublicRoute>
+                                <LoginPages />
+                            </PublicRoute>
+                        }
+                />
+               
+                <Route path="/*" element= {
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                } />
+
+                {/* cualquier ruta que no sea login  */}
+                {/* <Routes path="login" element={<LoginPages />} /> */}
+                 {/* <Route path="/*" element={<HeroesRoutes />} />     */}
+                
+            </Routes>
+        </>
+
+    )
+}
